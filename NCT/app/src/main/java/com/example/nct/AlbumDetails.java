59cb41fa2d22b08fb82ef1a29ed441416f3e9ebb
LinhDU <dulinh205@gmail.com>
2026-05-28 22:15:35 +0700

    add android project

---
+++ NCT/app/src/main/java/com/example/nct/AlbumDetails.java
@@ -0,0 +1,107 @@
+package com.example.nct;
+
+import static com.example.nct.MainActivity.musicFiles;
+
+import android.media.MediaMetadataRetriever;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class AlbumDetails extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ImageView albumPhoto;
+    String albumName;
+    ArrayList<MusicFiles> albumSongs = new ArrayList<>();
+    AlbumDetailsAdapter albumDetailsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_details);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        albumPhoto = findViewById(R.id.album_photo);
+
+        albumName = getIntent().getStringExtra("albumName");
+
+        int j = 0;
+
+        for (int i = 0; i < musicFiles.size(); i++) {
+
+            if (albumName.equals(musicFiles.get(i).getAlbum())) {
+
+                albumSongs.add(j, musicFiles.get(i));
+                j++;
+            }
+        }
+
+        // Kiểm tra tránh crash
+        if (albumSongs.size() > 0) {
+
+            byte[] image = getAlbumArt(albumSongs.get(0).getPath());
+
+            if (image != null) {
+
+                Glide.with(this)
+                        .load(image)
+                        .into(albumPhoto);
+
+            } else {
+
+                Glide.with(this)
+                        .load(R.drawable.nct_logo)
+                        .into(albumPhoto);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!(albumSongs.size() < 1)) {
+
+            albumDetailsAdapter =
+                    new AlbumDetailsAdapter(this, albumSongs);
+
+            recyclerView.setAdapter(albumDetailsAdapter);
+
+            recyclerView.setLayoutManager(
+                    new LinearLayoutManager(
+                            this,
+                            RecyclerView.VERTICAL,
+                            false
+                    )
+            );
+        }
+    }
+
+    private byte[] getAlbumArt(String uri) {
+
+        MediaMetadataRetriever retriever =
+                new MediaMetadataRetriever();
+
+        try {
+
+            retriever.setDataSource(uri);
+
+            byte[] art = retriever.getEmbeddedPicture();
+
+            retriever.release();
+
+            return art;
+
+        } catch (Exception e) {
+
+            return null;
+        }
+    }
+}
